a762fb1222d48916256ebcb6eeaceb5b207ad6dc
Fatiahmadi <fati.ahmadi66@gmail.com>
2016-07-30 14:15:26 +0200

    add FuctionComplexityCheck

---
+++ lua-checks/FunctionComplexityCheck.java
@@ -0,0 +1,70 @@
+/*
+ * SonarQube Flex Plugin
+ * Copyright (C) 2010-2016 SonarSource SA
+ * mailto:contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.flex.checks;
+
+import com.sonar.sslr.api.AstNode;
+import org.sonar.check.Priority;
+import org.sonar.check.Rule;
+import org.sonar.check.RuleProperty;
+import org.sonar.flex.FlexGrammar;
+import org.sonar.flex.api.FlexMetric;
+import org.sonar.flex.checks.utils.FlexCheck;
+import org.sonar.flex.checks.utils.Tags;
+import org.sonar.squidbridge.annotations.ActivatedByDefault;
+import org.sonar.squidbridge.annotations.SqaleLinearWithOffsetRemediation;
+import org.sonar.squidbridge.api.SourceFunction;
+
+@Rule(
+  key = "FunctionComplexity",
+  name = "Functions should not be too complex",
+  priority = Priority.MAJOR,
+  tags = Tags.BRAIN_OVERLOAD)
+@ActivatedByDefault
+@SqaleLinearWithOffsetRemediation(coeff = "1min", offset = "10min", effortToFixDescription = "per complexity point above the threshold")
+public class FunctionComplexityCheck extends FlexCheck {
+
+  private static final int DEFAULT_MAXIMUM_FUNCTION_COMPLEXITY_THRESHOLD = 10;
+
+  @RuleProperty(
+    key = "maximumFunctionComplexityThreshold",
+    description = "The maximum authorized complexity.",
+    defaultValue = "" + DEFAULT_MAXIMUM_FUNCTION_COMPLEXITY_THRESHOLD)
+  private int maximumFunctionComplexityThreshold = DEFAULT_MAXIMUM_FUNCTION_COMPLEXITY_THRESHOLD;
+
+  @Override
+  public void init() {
+    subscribeTo(FlexGrammar.FUNCTION_DEF, FlexGrammar.FUNCTION_EXPR);
+  }
+
+  @Override
+  public void leaveNode(AstNode node) {
+    SourceFunction function = (SourceFunction) getContext().peekSourceCode();
+    int functionComplexity = function.getInt(FlexMetric.COMPLEXITY);
+    if (functionComplexity > maximumFunctionComplexityThreshold) {
+      String message = String.format("Function has a complexity of %s which is greater than %s authorized.", functionComplexity, maximumFunctionComplexityThreshold);
+      createIssueWithCost(message, node, (double)functionComplexity - maximumFunctionComplexityThreshold);
+    }
+  }
+
+  public void setMaximumFunctionComplexityThreshold(int threshold) {
+    this.maximumFunctionComplexityThreshold = threshold;
+  }
+
+}
