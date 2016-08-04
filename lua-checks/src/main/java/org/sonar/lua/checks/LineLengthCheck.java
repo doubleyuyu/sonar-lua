80afed2a7f7fc97481cdc1d44309d559b5a40b09
Fatiahmadi <fati.ahmadi66@gmail.com>
2016-08-04 19:26:13 +0200

    Update LineLengthCheck.java

--- lua-checks/src/main/java/org/sonar/lua/checks/LineLengthCheck.java
+++ lua-checks/src/main/java/org/sonar/lua/checks/LineLengthCheck.java
@@ -1,6 +1,6 @@
 /*
  * SonarQube lua Plugin
- * Copyright (C) 2011-2016 SonarSource SA
+ * Copyright (C) 2016 SonarSource SA
  * mailto:contact AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
