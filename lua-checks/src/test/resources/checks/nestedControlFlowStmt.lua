a3640148b9bf12860acbeb1a0c608acf4f9fcd57
Fatiahmadi <fati.ahmadi66@gmail.com>
2016-08-04 21:46:04 +0200

    Update nestedControlFlowStmt.lua

--- lua-checks/src/test/resources/checks/nestedControlFlowStmt.lua
+++ lua-checks/src/test/resources/checks/nestedControlFlowStmt.lua
@@ -1,14 +1,14 @@
 function test()
   if (true) --level1
- then
-  if(true) --level2
- then
-   if (true)--level3
-then
-  if(false)--level4, Refactor this code to not nest more than 3
-then
-  end
-     end
-       end
-        end
-          end
+      then
+       if(true) --level2
+          then
+          if (true)--level3
+            then
+           if(false)--level4, Refactor this code to not nest more than 3
+          then
+              end
+               end
+                 end
+                    end
+                      end
